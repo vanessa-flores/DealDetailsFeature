8cfc1930b27bce08fe96a23cece387b72c4a0cd6
Vanessa Flores <risingdevhabits@gmail.com>
2023-09-30 09:44:13 -0500

    Initial Commit

---
+++ DealDetailsFeature/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DealDetailsFeature
+//
+//  Created by Vanessa Flores on 9/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DealDetailsFeature/DealDetailsFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealDetailsFeatureApp.swift
+//  DealDetailsFeature
+//
+//  Created by Vanessa Flores on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DealDetailsFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
